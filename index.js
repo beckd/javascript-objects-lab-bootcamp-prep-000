8ba2663017f56b42f83e3e2740efb931d1c51733
Dave Beck <dbeck2@outlook.com>
2018-06-14 16:31:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ var recipes = {
   sandwichB: 'turkey',
   sandwichC: 'tuna'
 };
+// Updating an Object (non-destructively)
+Object.assign({[sandwichD]: 'salami'}, recipes);
+return recipes;
+// Deleting an Object (non-destructively)
+var newObj2 = Object.assign({}, recipes);
+delete newObj2.sandwichB;
+// Delete object by key (destructively)
+delete recipes.sandwichA;
+// updating and object with key and value (destructively)
 recipes.sandwichE = 'roast beef';
 console.log(recipes)
